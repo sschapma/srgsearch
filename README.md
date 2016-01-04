c8c372b717da44341145205b011ddca6f644b5c2
Sam Chapman <schap843@gmail.com>
2016-01-04 21:50:21 +0900

    make code more readable

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# srgsearch
+# Seoul Restaurant Guide Search
+
+My first web app.
+Was built using Python, Flask, SQLite, HTML and CSS
+
+First step was to create a database of almost 500 restaurants in and around Seoul, South Korea.
+
+Next step was to create a python script to search the database by district, rating and type of food.
+
+Then, I read through several Flask tutorials to turn my script into a web app.
+
+Finally, I brushed up on CSS to style the app.
--- run.py
+++ run.py
@@ -6,15 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#Takes 3 variables from forms on index.html and returns them to results function
 @app.route('/', methods=['POST'])
 def my_form_post():
-
-    txt = request.form['district']
+    txt  = request.form['district']
     txt2 = request.form['rate']
     txt3 = request.form['type']
-    results(txt,txt2,txt3)
     return results(txt, txt2, txt3)
 
+#Connects to database and searches it using the variables from my_form_post
+#Displays results on results.html
 @app.route('/')
 def results(area, rating, food):
     conn = sqlite3.connect('SRG.db')
@@ -22,8 +23,7 @@ def results(area, rating, food):
     results = c.execute('SELECT * FROM restaurants WHERE address LIKE "%%%s%%" \
 		AND rating > %s-1 AND type LIKE "%%%s%%" ORDER BY rating DESC' \
 		% (area, rating, food,))
-    return render_template('results.html',
-                           results=results)
+    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
